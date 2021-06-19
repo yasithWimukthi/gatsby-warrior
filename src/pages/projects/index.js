53b605ff1ae0ef75e0bdb0356822782495163cef
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-19 19:44:11 +0530

    edit projects layout

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import Layout from "../../components/layout/layout.component";
 import '../../styles/global.css';
+import { Link, graphql } from 'gatsby'
 
-const Projects = () => {
+const Projects = ({ data }) => {
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
-      <div className="portfolio">
+      <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{ project.frontmatter.title }</h3>
+                <p>{ project.frontmatter.stack }</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   );
 }
 
 export default Projects;
+
+/**get project details from md files*/
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
