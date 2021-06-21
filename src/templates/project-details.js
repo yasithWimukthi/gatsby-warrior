b8855cb4bcad844b101f9d7b00c82b8bff2274c3
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-21 19:37:33 +0530

    final commit

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -2,20 +2,43 @@ import React from "react";
 import Img from "gatsby-image";
 import Layout from "../components/layout/layout.component";
 import '../styles/global.css';
+import { graphql } from 'gatsby';
 
-const ProjectDetails = () => {
+const ProjectDetails = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className="details">
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className="featured">
-          {/* <Img fluid={} /> */}
+          <Img fluid={featuredImg.childImageSharp.fluid} />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div className="styles.html" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
 }
 
 export default ProjectDetails;
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
